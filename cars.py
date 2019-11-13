bf9396a77d330eddfdc0ce040461a4fc4d20d258
Andrew Q <C15339351@mytudublin.ie>
2019-11-13 13:56:15 +0000

    A project I seen on Github + sutff I did

---
+++ cars.py
@@ -0,0 +1,7 @@
+cars = ["Audi", "BMW", "Honda", "Toyota", "Subaru"]
+
+for c in cars:
+    if c == "Honda":
+        print(c.upper())
+    else:
+        print(c.title())
---
+++ toppings.py
@@ -0,0 +1,4 @@
+req_toppings = ["Mushrooms",  "Onions", "Pineapple"]
+
+if "Mushrooms" in req_toppings:
+    print("hello")
