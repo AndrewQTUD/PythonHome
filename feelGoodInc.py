020561f8dabf0612dec4c53def5db3f16e2ddc42
Andrew Q <C15339351@mytudublin.ie>
2019-11-14 13:02:42 +0000

    worked on feelgoodinc

--- feelGoodInc.py
+++ feelGoodInc.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 from time import time, ctime
 
 cd = datetime.now()
+listOfEmails = ["andytown@live.ie", "andysempai12@gmail.com", "zalaru.raluca@gmail.com"]
+
 
 
     
@@ -19,16 +21,26 @@ def send_mail():
 
     subject = 'You can do it Raluca!'
 
-    body = 'This is an automated email to remind you that you can do anything! ' + str(cd)
+    body = 'This is an automated email to remind you that you can do anything! ' + str(cd) + ' There are : ' 
+    + str(len(listOfEmails)) + ' other in the e-mail list'
 
     msg = f"Subject : {subject}\n\n{body}"
 
     server.sendmail(
-        #Test e-mail
+        #Sender
         'andysempai12@gmail.com',
-        #Another test e-mail
+        #recipent
         'andytown@live.ie',
-        #Destination e-mail
+        
+        msg
+    )
+
+    server.sendmail(
+        #Sender
+        'andysempai12@gmail.com',
+        #recipent
+        'zalaru.raluca@gmail.com',
+        
         msg
     )
     print("e-mail sent " + str(cd))
