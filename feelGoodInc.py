2b4ef4870474115cddced4b0d6e22afc7c7f85c4
Andrew Q <C15339351@mytudublin.ie>
2019-11-19 23:49:17 +0000

    added ability to enter in subject + body from terminal

--- feelGoodInc.py
+++ feelGoodInc.py
@@ -4,48 +4,70 @@ import smtplib
 from datetime import datetime, timedelta
 from time import time, ctime
 
+
+
+
 cd = datetime.now()
 listOfEmails = ["andytown@live.ie", "andysempai12@gmail.com", "zalaru.raluca@gmail.com"]
 
 
 
+print('Enter your subject line.')
+subject = input()
+
+
+
+print('Enter the body of your message.')
+body = input()
+
+body = body + ' ' + str(cd)
+
+
+
+
+
     
-
-def send_mail():
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
+def transferInfo(body, subject):
+    def send_mail():
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
     
-    server.login('andysempai12@gmail.com', 'ocbhdmemnkvkvzea')
+        server.login('andysempai12@gmail.com', 'ocbhdmemnkvkvzea')
 
-    subject = 'You can do it Raluca!'
-
-    body = 'This is an automated email to remind you that you can do anything! ' + str(cd) + ' There are : ' 
-    + str(len(listOfEmails)) + ' other in the e-mail list'
-
-    msg = f"Subject : {subject}\n\n{body}"
-
-    server.sendmail(
-        #Sender
-        'andysempai12@gmail.com',
-        #recipent
-        'andytown@live.ie',
         
-        msg
-    )
 
-    server.sendmail(
-        #Sender
-        'andysempai12@gmail.com',
-        #recipent
-        'zalaru.raluca@gmail.com',
         
-        msg
-    )
-    print("e-mail sent " + str(cd))
+        
+        msg = f"Subject : {subject}\n\n{body}"
 
-    server.quit()
+        server.sendmail(
+            #Sender
+            'andysempai12@gmail.com',
+            #recipent
+            'andytown@live.ie',
 
-send_mail()    
+            
+        
+            msg
+        )
+
+        server.sendmail(
+            #Sender
+            'andysempai12@gmail.com',
+            #recipent
+            'zalaru.raluca@gmail.com',
+        
+         msg
+        )
+
+        
+        print("e-mail sent " + str(cd))
+
+        server.quit()
+
+    send_mail()   
+transferInfo(body,subject) 
+
     
